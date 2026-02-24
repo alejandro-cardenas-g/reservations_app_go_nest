729fcc8f25e6ffa2fa74de344418bdd30b2379af
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2026-02-23 21:57:10 -0500

    Enhance AuthController to manage refresh token cookies
    - Updated AuthController to include ConfigService for cookie management.
    - Modified callback method to set refresh token cookies upon successful authentication.
    - Added private method to handle cookie settings, ensuring secure and appropriate configurations based on environment.

--- api/src/auth/controllers/auth.controller.ts
+++ api/src/auth/controllers/auth.controller.ts
@@ -4,18 +4,24 @@ import {
   Get,
   Post,
   Query,
+  Res,
   UnauthorizedException,
 } from '@nestjs/common';
 import { LoginDto } from '../dtos/login.dto';
 import { Oauth2Service } from '../services/oauth2.service';
 import { CallbackDto } from '../dtos/callback.dto';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @Controller({
   path: 'auth',
   version: '1',
 })
 export class AuthController {
-  constructor(private readonly oauth2Service: Oauth2Service) {}
+  constructor(
+    private readonly oauth2Service: Oauth2Service,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('login')
   login(@Body() body: LoginDto) {
@@ -28,12 +34,37 @@ export class AuthController {
   }
 
   @Get('callback')
-  async callback(@Query() query: CallbackDto) {
+  async callback(
+    @Query() query: CallbackDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const { code, state } = query;
     const result = await this.oauth2Service.callback(code, state);
 
     if (!result.isSuccess) throw new UnauthorizedException(result.error);
 
+    this.setRefreshTokenCookie(
+      res,
+      result.value.refreshToken,
+      result.value.refreshTokenExpiresAt,
+    );
+
     return { tokens: result.value };
+  }
+
+  private setRefreshTokenCookie(
+    res: Response,
+    refreshToken: string,
+    refreshTokenExpiresAt: Date,
+  ) {
+    const cookieName = this.configService.getOrThrow<string>('COOKIE_NAME');
+    const isProduction =
+      this.configService.getOrThrow<string>('NODE_ENV') === 'production';
+    res.cookie(cookieName, refreshToken, {
+      httpOnly: true,
+      expires: refreshTokenExpiresAt,
+      sameSite: isProduction ? 'none' : undefined,
+      secure: isProduction,
+    });
   }
 }
